d654802b954f183528e951c6b2839789fb19f7f5
Elepi <elenakarenf@gmail.com>
2020-10-03 15:43:47 -0600

    Calculo metodo para el prestamo

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const express = require("express");
 // Importar handlebars como template engine
 const exphbs = require("express-handlebars");
 // Importar body parser que nos permite acceder al cuerpo de la petición HTTP
-/*const bodyParser = require("body-parser");
+const bodyParser = require("body-parser");
 // Importar la función de cálculo de método francés
 const { calcularMetodoFrances } = require("./calculoMetodoFrances");
-*/
+
 // Crear un servidor express
 const app = express();
 
@@ -16,7 +16,7 @@ app.engine("hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }));
 app.set("view engine", "hbs");
 
 // Habilitar body parse para leer los datos del cuerpo de peticiones POST
-//app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // Crear una ruta para /
 // Información sobre los verbos HTTP
@@ -24,7 +24,7 @@ app.set("view engine", "hbs");
 app.get("/", (req, res, next) => {
   res.render("formulario_prestamo");
 });
-/*
+
 app.post("/prestamo", (req, res, next) => {
   // Asignación por destructuring
   // https://developer.mozilla.org/es/docs/Web/JavaScript/Referencia/Operadores/Destructuring_assignment
@@ -33,8 +33,7 @@ app.post("/prestamo", (req, res, next) => {
   calcularMetodoFrances(monto, tasaInteres, periodo);
 
   res.send("Petición aceptada");
-});*/
-
+});
 // Inicializar el servidor en un puerto en específico
 app.listen(5000, () => {
   console.log("Servidor ejecutándose en el puerto 5000");
