74b2f5fbecf46e146d6834bb0dc4f54606b0fb63
Elepi <elenakarenf@gmail.com>
2020-10-03 22:20:03 -0600

    creacion del ciclo for para calculo del prestamo

--- calculoMetodoFrances.js
+++ calculoMetodoFrances.js
@@ -7,6 +7,7 @@ exports.calcularMetodoFrances = (capital, tasaInteres, plazo) => {
     let mes = 0;
     let interes = 0;
     let tasaInteresDecimal = tasaInteres / 100;
+    let cuotadeuda =0;
     const cuotas = [];
   
     // Transformar los años en meses
@@ -17,5 +18,24 @@ exports.calcularMetodoFrances = (capital, tasaInteres, plazo) => {
       (capital * tasaInteresDecimal) / (1 - (1 + tasaInteresDecimal) ** -plazo);
   
     cuota = cuota.toFixed(2);
-    console.log(cuota);
+  
+   // console.log(cuota);
+
+  
+
+let num;
+   console.log('│'+"Mes"+'│ '+"cuota"+' │'+interes+' │ '+cuota+' │ '+capital +' │ ' );
+   for (let i = 1; i <= plazo; i++) {
+       interes = capital * tasaInteresDecimal;
+       cuotadeuda = cuota - interes; 
+       capital = capital - cuotadeuda;
+       interes = interes.toFixed(2);
+       capital = capital.toFixed(2);
+       cuotadeuda = cuotadeuda .toFixed(3);
+       num = i;
+       console.log('│ '+num+'│ '+cuotadeuda+' │'+interes+' │ '+cuota+' │ '+capital +' │ ' );
+     
+     }
+ 
   };
+ 
